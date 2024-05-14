80dc9df5fa1f25250ec58edec6c4c8fb9fd60351
Mikahaftamu <123436236+Mikahaftamu@users.noreply.github.com>
2024-05-13 22:27:08 -0700

    Test Case for the first method(FromMeter method)

---
+++ LengthConverterTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LengthConverterTest {
+    
+    public LengthConverterTest(){
+
+    }
+    
+    @Test
+    public void testConvertFromMeters() {
+        double lengthInMeters = 10.0;
+
+        Assertions.assertEquals(110.0, LengthConverter.convertFromMeters(lengthInMeters, "m"));
+        Assertions.assertEquals(0.01, LengthConverter.convertFromMeters(lengthInMeters, "cm"));
+        Assertions.assertEquals(10000.0, LengthConverter.convertFromMeters(lengthInMeters, "mm"));
+        Assertions.assertEquals(0.00621371, LengthConverter.convertFromMeters(lengthInMeters, "mi"), 0.000001);
+        Assertions.assertEquals(32.80839895, LengthConverter.convertFromMeters(lengthInMeters, "ft"), 0.000001);
+        Assertions.assertEquals(393.7007874, LengthConverter.convertFromMeters(lengthInMeters, "in"), 0.000001);
+        Assertions.assertEquals(10.93613298, LengthConverter.convertFromMeters(lengthInMeters, "yd"), 0.000001); 
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> LengthConverter.convertFromMeters(lengthInMeters, "invalid"));
+    }
+}
