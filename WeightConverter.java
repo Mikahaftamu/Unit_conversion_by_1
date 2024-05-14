f6623d2bf2307e2947b9d63247c68cedb1de3890
Binyam Tesfay <binyamtesfay93@gmail.com>
2024-05-14 11:24:21 -0700

    first method for weight conversion

---
+++ WeightConverter.java
@@ -0,0 +1,9 @@
+public class WeightConverter {
+    public static double kilogramToGram(double kilogram) {
+        return kilogram * 1000;
+    }
+
+    public static double gramToKilogram(double gram) {
+        return gram / 1000;
+    }
+}
