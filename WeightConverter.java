d4a8b325e710cf0edfd4a54d2d5230e66f480c2d
Binyam Tesfay <binyamtesfay93@gmail.com>
2024-05-14 22:58:03 -0700

    It convert milligram to killogram and vice versa

--- WeightConverter.java
+++ WeightConverter.java
@@ -13,5 +13,13 @@ public class WeightConverter {
     public static double gramToMilligram(double gram) {
         return gram * 1000;
     }
+    public static double milligramToKilogram(double milligram) {
+        return milligram / 1_000_000;
+    }
+
+    public static double kilogramToMilligram(double kilogram) {
+        return kilogram * 1_000_000;
+    }
+
 
 }
