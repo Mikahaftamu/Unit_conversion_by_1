a6742b251b6142c04b4cfbce95eb4e2e351b847a
Betelihemaraya <betelihemarayakassa@gmail.com>
2024-05-15 06:26:00 +0300

    adding first Time conversion method

---
+++ TimeConversion.java
@@ -0,0 +1,11 @@
+// package time.test;
+public class TimeConversion {
+    public static int convertSecondsToMinutes(int seconds) {
+        return seconds / 60;
+    }
+
+    public static int convertSecondsToHours(int seconds) {
+        int minutes = seconds / 60;
+        return minutes / 60;
+    }
+}
