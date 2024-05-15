700bbd1efdc4cb73d4cb3ce96a39b77c94a9bdeb
Betelihemaraya <betelihemarayakassa@gmail.com>
2024-05-15 15:41:39 +0300

    adding another two methods for followed 2 methods

--- TimeConversion.java
+++ TimeConversion.java
@@ -16,4 +16,12 @@ public class TimeConversion {
     public static int convertMinutesToHours(int minutes) {
         return minutes / 60;
     }
+    
+    public static int convertHoursToMinutes(int hours) {
+        return hours * 60;
+    }
+
+    public static int convertHoursToSeconds(int hours) {
+        return hours * 3600;
+    }
 }
