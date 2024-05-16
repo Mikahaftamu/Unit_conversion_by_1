deb5c064119acbc27964b6c0b9478afff973905e
Binyam Tesfay <binyamtesfay93@gmail.com>
2024-05-16 16:09:37 -0700

    add the first methods

--- src/main/WeightConverter.java
+++ src/main/WeightConverter.java
@@ -1,6 +1,12 @@
- package main;
+package main;
 
 public class WeightConverter {
 
-    
+    public static double kilogramToGram(double kilogram) {
+        return kilogram * 1000;
+    }
+
+    public static double gramToKilogram(double gram) {
+        return gram / 1000;
+    }
 }
