89034a7bd25038a203420656d40c4448abaa2075
Mikahaftamu <123436236+Mikahaftamu@users.noreply.github.com>
2024-05-16 02:32:55 -0700

    adding Testcase for convertFromKilometer method

--- src/test/LengthConverterTest.java
+++ src/test/LengthConverterTest.java
@@ -28,4 +28,22 @@ public class LengthConverterTest {
     
     }
 
+    @Test
+    public void testConvertFromKilo() {
+        double lengthkilo = 10.0;
+
+        double actual5 =LengthConverter.convertFromKilometer(lengthkilo, "m");
+        double expected5= 0.01;
+        assertEquals(expected5, actual5, 0.01);
+
+        
+        double actual1 =LengthConverter.convertFromKilometer(lengthkilo, "cm");
+        double expected1= 1000.0;
+        assertEquals(expected1, actual1, 0.01);
+
+        double actual2 =LengthConverter.convertFromKilometer(lengthkilo, "mm");
+        double expected2= 10000.0;
+        assertEquals(expected2, actual2, 0.01);
+
+    }
 }
